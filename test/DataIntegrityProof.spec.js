f021f965cc79dfb2a0c50154179c72ddffd62811
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-11-06 08:19:45 -0500

    Fix message.

--- test/DataIntegrityProof.spec.js
+++ test/DataIntegrityProof.spec.js
@@ -42,7 +42,6 @@ describe('DataIntegrityProof', () => {
       } catch(e) {
         error = e;
       }
-
       const errorMessage = `The signer's algorithm "${signer.algorithm}" ` +
         `does not match the required algorithm for the cryptosuite ` +
         `"${eddsa2022CryptoSuite.requiredAlgorithm}".`;
