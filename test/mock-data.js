e30fc5961734e551cbc03b50a1cd74efdc8c8f2d
Andrew Jones <ajones@digitalbazaar.com>
2024-09-01 18:27:37 -0400

    Add signedCredInvalidCreated to mock.data.js.

--- test/mock-data.js
+++ test/mock-data.js
@@ -67,3 +67,33 @@ export const credentialWithLegacyContext = {
     alumniOf: 'Example University'
   }
 };
+
+export const signedCredentialWithInvalidCreated = {
+  '@context': [
+    'https://www.w3.org/2018/credentials/v1',
+    {
+      AlumniCredential: 'https://schema.org#AlumniCredential',
+      alumniOf: 'https://schema.org#alumniOf'
+    },
+    'https://w3id.org/security/data-integrity/v2'
+  ],
+  id: 'http://example.edu/credentials/1872',
+  type: [
+    'VerifiableCredential',
+    'AlumniCredential'
+  ],
+  issuer: 'https://example.edu/issuers/565049',
+  issuanceDate: '2010-01-01T19:23:24Z',
+  credentialSubject: {
+    id: 'https://example.edu/students/alice',
+    alumniOf: 'Example University'
+  },
+  proof: {
+    created: 'May-23-2022',
+    type: 'DataIntegrityProof',
+    verificationMethod: 'https://example.edu/issuers/565049#z6MkwXG2WjeQnNxSoynSGYU8V9j3QzP3JSqhdmkHc6SaVWoT',
+    cryptosuite: 'eddsa-2022',
+    proofPurpose: 'assertionMethod',
+    proofValue: 'z4sKdR9XJ1CuUKTzjRqWDZXTPr6HRwPaLTkqw9Co3RXmDGsyfbs5czpzaMridAyd4Kq14hW5rxHuEjkMMpVcNTZN4'
+  }
+};
